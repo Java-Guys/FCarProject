b877418481bd4a4be3a129231c490635473c2e01
M-Hamdy-M <mhmd2910@yahoo.co.uk>
2020-10-10 23:14:40 +0300

    oa created Payment and Invoice classes

--- src/model/Invoice.java
+++ src/model/Invoice.java
@@ -1,4 +1,141 @@
 package model;
 
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @author Omar Alkashef
+ * Creation Date : 10-10-2020
+ * @version 1
+ */
+
 public class Invoice {
+
+    private int invoiceNo;
+    private Date invoiceDate;
+    private List<Payment> payments;
+
+    /**
+     * @param invoiceNo
+     * @param invoiceDate
+     * @param payments
+     */
+    public Invoice(int invoiceNo, Date invoiceDate, List<Payment> payments) {
+        this.invoiceNo = invoiceNo;
+        this.invoiceDate = invoiceDate;
+        this.payments = payments;
+    }
+
+    /**
+     * @return invoiceNo
+     */
+    public int getInvoiceNo() {
+        return invoiceNo;
+    }
+
+    /**
+     * @param invoiceNo
+     */
+    public void setInvoiceNo(int invoiceNo) {
+        this.invoiceNo = invoiceNo;
+    }
+
+    /**
+     * @return invoiceDate
+     */
+    public Date getInvoiceDate() {
+        return invoiceDate;
+    }
+
+    /**
+     * @param invoiceDate
+     */
+    public void setInvoiceDate(Date invoiceDate) {
+        this.invoiceDate = invoiceDate;
+    }
+
+    /**
+     * @return payments
+     */
+    public List<Payment> getPayments() {
+        return payments;
+    }
+
+    /**
+     * @param payments
+     */
+    public void setPayments(List<Payment> payments) {
+        this.payments = payments;
+    }
+
+    /**
+     * @param payment
+     * @return message
+     */
+    public String modifyPayment(Payment payment) {
+
+        for (int i = 0; i < payments.size(); i++) {
+            if (payments.get(i).getPaymentId() == payment.getPaymentId()) {
+                payments.set(i, payment);
+                return "Updated payment successfully";
+            }
+
+        }
+        return "Couldn't update the payment";
+    }
+
+    /**
+     * @param paymentId
+     * @return message
+     */
+    public String deletePayment(int paymentId) {
+
+        for (int i = 0; i < payments.size(); i++) {
+            if (payments.get(i).getPaymentId() == paymentId) {
+                payments.remove(i);
+                return "Delete payment successfully";
+            }
+        }
+        return "Couldn't delete the payment";
+    }
+
+    /**
+     * @param payment
+     * @return message
+     */
+    public String addPayment(Payment payment) {
+        for (int i = 0; i < payments.size(); i++) {
+            if (payments.get(i).getPaymentId() == payment.getPaymentId()) {
+
+                return "This payment is already added";
+            }
+        }
+        payments.add(payment);
+        return "Add payment successfully";
+    }
+
+    /**
+     * @param paymentId
+     * @return payment
+     */
+    public Payment getPayment(int paymentId){
+        for(int i = 0; i < payments.size(); i++){
+            if(payments.get(i).getPaymentId() == paymentId){
+                return payments.get(i);
+            }
+        }
+        return null;
+    }
+
+    /**
+     * @return Total Payment;
+     */
+    public double calculateTotalPayment(){
+        double total = 0.0;
+        for(int i = 0; i < payments.size(); i++){
+             total += payments.get(i).getPrice();
+        }
+        return total;
+    }
+
 }
--- src/model/Payment.java
+++ src/model/Payment.java
@@ -1,4 +1,86 @@
 package model;
 
+import java.util.Date;
+
+/**
+ * @author Omar Alkashef
+ * Creation Date : 10-10-2020
+ * @version 1
+ */
+
 public class Payment {
+
+    private String paymentDescription;
+    private int paymentId;
+    private double price;
+    private Date paymentDate;
+
+    /**
+     * @param paymentDescription
+     * @param paymentId
+     * @param price
+     * @param paymentDate
+     */
+    public Payment(String paymentDescription, int paymentId, double price, Date paymentDate) {
+        this.paymentDescription = paymentDescription;
+        this.paymentId = paymentId;
+        this.price = price;
+        this.paymentDate = paymentDate;
+    }
+
+    /**
+     * @return paymentDescription
+     */
+    public String getPaymentDescription() {
+        return paymentDescription;
+    }
+
+    /**
+     * @param paymentDescription
+     */
+    public void setPaymentDescription(String paymentDescription) {
+        this.paymentDescription = paymentDescription;
+    }
+
+    /**
+     * @return paymentId
+     */
+    public int getPaymentId() {
+        return paymentId;
+    }
+
+    /**
+     * @param paymentId
+     */
+    public void setPaymentId(int paymentId) {
+        this.paymentId = paymentId;
+    }
+
+    /**
+     * @return price
+     */
+    public double getPrice() {
+        return price;
+    }
+
+    /**
+     * @param price
+     */
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    /**
+     * @return paymentDate
+     */
+    public Date getPaymentDate() {
+        return paymentDate;
+    }
+
+    /**
+     * @param paymentDate
+     */
+    public void setPaymentDate(Date paymentDate) {
+        this.paymentDate = paymentDate;
+    }
 }
