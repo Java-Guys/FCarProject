e1b17b1cdf39522885a07b219ae99b7f444324ca
ezzgaber <ea1803302@qu.edu.qa>
2020-10-10 22:33:18 +0300

    EA create Customer , Resident and Visitor classes

--- src/model/Visitor.java
+++ src/model/Visitor.java
@@ -1,4 +1,86 @@
 package model;
 
-public class Visitor {
+
+import java.time.LocalDate;
+
+/**
+ * @author : Ezeldin Ahmed
+ * Date    : 10-10-2020
+ * version : 1
+ */
+public class Visitor extends Customer {
+    private int passportNo;
+    private LocalDate entryDate;
+    private LocalDate visaExpiryDate;
+
+    /**
+     * @param customerId
+     * @param name
+     * @param phone
+     * @param address
+     * @param nationality
+     * @param drivingLicence
+     * @param passportNo
+     * @param entryDate
+     * @param visaExpiryDate
+     */
+    public Visitor(int customerId,
+                   String name,
+                   String phone,
+                   String address,
+                   String nationality,
+                   String drivingLicence,
+                   int passportNo,
+                   LocalDate entryDate,
+                   LocalDate visaExpiryDate) {
+        super(customerId, name, phone, address, nationality, drivingLicence);
+        this.passportNo = passportNo;
+        this.entryDate = entryDate;
+        this.visaExpiryDate = visaExpiryDate;
+    }
+
+    public Visitor() {
+    }
+
+    /**
+     * @return passportNo
+     */
+    public int getPassportNo() {
+        return passportNo;
+    }
+
+    /**
+     * @param passportNo
+     */
+    public void setPassportNo(int passportNo) {
+        this.passportNo = passportNo;
+    }
+
+    /**
+     * @return Date
+     */
+    public LocalDate getEntryDate() {
+        return entryDate;
+    }
+
+    /**
+     * @param entryDate
+     */
+    public void setEntryDate(LocalDate entryDate) {
+        this.entryDate = entryDate;
+    }
+
+    /**
+     * @return visaExpiryDate
+     */
+    public LocalDate getVisaExpiryDate() {
+        return visaExpiryDate;
+    }
+
+    /**
+     * @param visaExpiryDate
+     */
+    public void setVisaExpiryDate(LocalDate visaExpiryDate) {
+        this.visaExpiryDate = visaExpiryDate;
+    }
 }
