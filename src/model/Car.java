b37a83e4c1ab883270410bd6ed1f5ad6c2c10609
Mahmoud Mohd <mt1908144@qu.edu.qa>
2020-10-14 17:16:28 +0300

    mt creating CarType enum and Car class

--- src/model/Car.java
+++ src/model/Car.java
@@ -1,4 +1,60 @@
 package model;
 
-public class Car {
+/**
+ *
+ * @author Mahmoud Shreif
+ * Date: 10/10/2020
+ * Version 1
+ *
+ */
+class Car {
+    private String plateNo;
+    private String model;
+    private boolean isAvailable;
+    private CarType type;
+
+    public Car(String plateNo, String model, boolean isAvailable, CarType type) {
+        this.plateNo = plateNo;
+        this.model = model;
+        this.isAvailable = isAvailable;
+        this.type = type;
+
+    }
+
+    public Car() {
+
+    }
+
+    public String getPlateNo() {
+        return plateNo;
+    }
+
+    public void setPlateNo(String plateNo) {
+        this.plateNo = plateNo;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean isAvailable) {
+        this.isAvailable = isAvailable;
+    }
+
+    public CarType getType() {
+        return type;
+    }
+
+    public void setType(CarType type) {
+        this.type = type;
+    }
+
 }
--- src/model/CarType.java
+++ src/model/CarType.java
@@ -1,4 +1,14 @@
 package model;
-
-public class CarType {
+/**
+ *
+ * @author Mahmoud Shreif
+ * Date: 10/10/2020
+ * Version 1
+ *
+ */
+public enum CarType {
+    SEDAN,
+    SUV,
+    VAN,
+    TRUCK;
 }
